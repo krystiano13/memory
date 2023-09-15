4763c886fa0357703950d5427232242f26108258
krystiano13 <crystianotv@gmail.com>
2023-09-15 16:11:13 +0200

    implement revealing cards

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -2,12 +2,23 @@ import { useState, useEffect } from "preact/hooks";
 import type { cardType, cards } from "./types";
 import { colors } from "./data";
 import "./index.css";
+import "./colors.css";
 
 export function Page() {
   const [cards, setCards] = useState<cards>([]);
   const [percentage, setPercentage] = useState<number>(0);
   const [guesses, setGuesses] = useState<number>(0);
   const [matches, setMatches] = useState<number>(0);
+  const [cardsRevealed, setCardsRevealed] = useState<number>(0);
+
+  const revealCard = (e) => {
+    const cardsArray = cards;
+    if (cardsRevealed >= 2) return;
+    if (e.target.className !== "card") return;
+
+    setCardsRevealed((prev) => prev + 1);
+    cardsArray[Number(e.target.id)].revealed = true;
+  };
 
   const renderCards = () => {
     const cardsArray: cards = [];
@@ -55,7 +66,14 @@ export function Page() {
     <div class="wrapper">
       <div class="cards">
         {cards.map((item) => (
-          <div key={item.id} class={`card ${item.colorName}`}></div>
+          <div
+            id={String(item.id)}
+            key={item.id}
+            class={
+              item.matched || item.revealed ? `card ${item.colorName}` : `card`
+            }
+			onClick={(e) => revealCard(e)}
+          ></div>
         ))}
       </div>
       <div class="info">
