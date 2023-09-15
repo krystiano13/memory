8a82adbae31d0e32b24aa022b67107e9289e3c69
krystiano13 <crystianotv@gmail.com>
2023-09-15 13:03:18 +0200

    implement creating cards array

--- pages/index/index.page.tsx
+++ pages/index/index.page.tsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from "preact/hooks";
+import type { cardType, cards } from "./types";
+import "./index.css";
 
 export function Page() {
-	return (
-		<div>
-			
-		</div>
-	);
-}
+  const [cards, setCards] = useState<cards>([]);
 
+  const renderCards = () => {
+    const cardsTemplate: cards = [];
+
+    for (let i = 0; i < 16; i++) {
+      cardsTemplate.push({
+        id: i,
+        colorName: "",
+        matched: false,
+        revealed: false,
+      });
+    }
+
+    setCards(cardsTemplate);
+  };
+
+  useEffect(() => {
+    renderCards();
+  }, []);
+
+  return (
+    <div class="wrapper">
+      <div class="cards">
+        {cards.map((item) => (
+          <p>{item.id}</p>
+        ))}
+      </div>
+      <div class="info"></div>
+      <div class="buttons"></div>
+    </div>
+  );
+}
---
+++ pages/index/types.ts
@@ -0,0 +1,8 @@
+export type cardType = {
+    id: number,
+    colorName: string,
+    revealed: boolean,
+    matched: boolean
+};
+
+export type cards = cardType[];
